9fc7dbfc5c2739ae0938b194ada2a82eff601362
Valeria Veverita <36117142+ValeriaVeverita@users.noreply.github.com>
2018-12-03 11:05:38 -0500

    Initial commit

---
+++ FindMinValueVV/FindMinValueVV/MinValueForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FindMinValueVV
+{
+    partial class frmMinValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstValues = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(114, 41);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(89, 37);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstValues
+            // 
+            this.lstValues.FormattingEnabled = true;
+            this.lstValues.Location = new System.Drawing.Point(94, 93);
+            this.lstValues.Name = "lstValues";
+            this.lstValues.Size = new System.Drawing.Size(131, 147);
+            this.lstValues.TabIndex = 1;
+            // 
+            // frmMinValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 332);
+            this.Controls.Add(this.lstValues);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMinValue";
+            this.Text = "Min Value by Valeria V";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstValues;
+    }
+}
+
---
+++ FindMinValueVV/FindMinValueVV/MinValueForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindMinValueVV
+{
+    public partial class frmMinValue : Form
+    {
+        Random randomNumGen = new Random();
+        public frmMinValue()
+        {
+            InitializeComponent();
+           
+        }
+
+        private int FindMinVal(int[]array)
+        {
+            int minVal = 501;
+            foreach (int value in array)
+            {
+                if (value<minVal)
+                {
+                    minVal = value;
+                }
+            }
+            return minVal;
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int[] valueArray = new int[10];
+            int value;
+            lstValues.Items.Clear();
+
+            for (int counter = 0; counter < 10; counter ++)
+            {
+                value = randomNumGen.Next(1, 501);
+                valueArray[counter] = value;
+                lstValues.Items.Add(value);
+
+                this.Refresh();
+
+            }
+
+            int displayVal = FindMinVal(valueArray);
+            MessageBox.Show("The minimum value is " + displayVal);
+        }
+    }
+}
