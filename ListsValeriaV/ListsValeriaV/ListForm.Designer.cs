2567944fb8bac75108f3285e39edeca348b4d090
ValeriaVeverita <valeria.veverita@ocsbstudent.ca>
2018-12-05 14:26:00 -1100

    Changes

---
+++ ListsValeriaV/ListsValeriaV/ListForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ListsValeriaV
+{
+    partial class frmList
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lstMarks = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblQuestion.ForeColor = System.Drawing.Color.Peru;
+            this.lblQuestion.Location = new System.Drawing.Point(18, 36);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(278, 25);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Enter a mark (between 1 and 100)";
+            // 
+            // txtMark
+            // 
+            this.txtMark.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMark.Location = new System.Drawing.Point(117, 68);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(72, 32);
+            this.txtMark.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.BackColor = System.Drawing.Color.Peru;
+            this.btnEnter.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEnter.ForeColor = System.Drawing.Color.White;
+            this.btnEnter.Location = new System.Drawing.Point(115, 106);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 37);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = false;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lstMarks
+            // 
+            this.lstMarks.FormattingEnabled = true;
+            this.lstMarks.Location = new System.Drawing.Point(94, 149);
+            this.lstMarks.Name = "lstMarks";
+            this.lstMarks.Size = new System.Drawing.Size(120, 147);
+            this.lstMarks.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.Peru;
+            this.btnCalculate.Font = new System.Drawing.Font("Impact", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.ForeColor = System.Drawing.Color.White;
+            this.btnCalculate.Location = new System.Drawing.Point(106, 302);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(97, 37);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Average";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 349);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lstMarks);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtMark);
+            this.Controls.Add(this.lblQuestion);
+            this.Name = "frmList";
+            this.Text = "List by Valeria V";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.ListBox lstMarks;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ ListsValeriaV/ListsValeriaV/ListForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListsValeriaV
+{
+    public partial class frmList : Form
+    {
+        //create global variables
+        int numOfVal = 0;
+        List<int> userValues = new List<int>();
+        public frmList()
+        {
+            InitializeComponent();
+        }
+
+        //Function: CalculateAverage
+        //Input: List<int>tmpList
+        //Output: int average
+        //Description this function calculates the average
+        private int CalculateAverage(List<int>tmpList, int listElem)
+        {
+            //declare local variables
+            int average = 0;
+            int sum = 0;
+            //loop through each element in the list 
+            for (int index = 0; index < tmpList.Count; index = 0)
+            {
+                //calculate the sum of the elemts in the list
+                sum = sum + tmpList[index];
+                Console.WriteLine(sum);
+            }
+            //divide the sum by the number of elements in the list to find the average
+            average = sum / listElem;
+            Console.WriteLine(average);
+            //return the average
+            return average;
+        }
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int userMark = 0;
+
+            //increment the number of values
+            numOfVal++;
+
+            //convert the value from the text box to an integer
+            try
+            {
+                userMark = int.Parse(txtMark.Text);
+            }
+            //catch if the value from the text box is not an integer and write an error to the console
+            catch(Exception parseError)
+            {
+                Console.WriteLine("An error occured:{0}", parseError);
+            }
+
+            //check if the user entered a number between 1 and 100
+            if ((userMark >= 1) && (userMark<= 100))
+            {
+                //add the mark to the list box
+                this.lstMarks.Items.Add(userMark);
+                //add the mark to the list
+                userValues.Add(userMark);
+            }
+            //if the number is not between 1 and 100, display an error to the message box
+            else
+            {
+                MessageBox.Show("Please enter a number between 1 and 100");
+            }
+
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //declare local variables
+            int average;
+            //disable the enter button
+            btnEnter.Enabled = false;
+            //call the calculate average function
+            average = CalculateAverage(userValues, numOfVal);
+            //display the average in a message box
+            MessageBox.Show("The average is " + average);
+
+        }
+    }
+}
